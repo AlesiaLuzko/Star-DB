911703981e2120d9dafe5980c3b2a70c60f0d3aa
Alesia Luzko <lesya.grishko@gmail.com>
2024-01-09 17:58:28 +0100

    Create new components "LoginPage" and "SecretPage"

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,29 +8,57 @@ import {
 
 import ErrorBoundary from "../error-boundary";
 import SwapiService from "../../services/swapi-servise";
+import Layout from "../layout";
 import { SwapiServiceProvider } from "../swapi-service-context";
-import { PeoplePage, PlanetsPage, StarshipsPage } from "../pages";
 import { StarshipDetails } from "../sw-components";
+import {
+  PeoplePage,
+  PlanetsPage,
+  StarshipsPage,
+  LoginPage,
+  SecretPage
+} from "../pages";
 
 import './app.css';
 
-import Layout from "../layout";
-
-const router = createBrowserRouter(createRoutesFromElements(
-  <Route path="/" element={<Layout />}>
-    <Route path="people" element={<PeoplePage />} />
-    <Route path="people/:id" element={<PeoplePage />} />
-    <Route path="planets" element={<PlanetsPage />} />
-    <Route path="starships" element={<StarshipsPage />} />
-    <Route path="starships/:id"
-           element={<StarshipDetails /> } />
-  </Route>
-));
-
 export default class App extends Component {
+
+  state = {
+    isLoggedIn: false
+  };
+
+  onLogin = () => {
+    this.setState({
+      isLoggedIn: true
+    });
+  };
+
   swapiService = new SwapiService();
 
   render() {
+    const { isLoggedIn } = this.state;
+
+    const router = createBrowserRouter(createRoutesFromElements(
+      <Route path="/" element={<Layout />}>
+        <Route path="people" element={<PeoplePage />} />
+        <Route path="people/:id" element={<PeoplePage />} />
+        <Route path="planets" element={<PlanetsPage />} />
+        <Route path="starships" element={<StarshipsPage />} />
+        <Route path="starships/:id"
+               element={<StarshipDetails /> } />
+        <Route
+          path="login"
+          element={<LoginPage
+            isLoggedIn={isLoggedIn}
+            onLogin={this.onLogin}/>}
+        />
+        <Route
+          path="secret"
+          element={<SecretPage isLoggedIn={isLoggedIn} />}
+        />
+      </Route>
+    ));
+
     return (
       <ErrorBoundary>
         <SwapiServiceProvider value={this.swapiService}>
