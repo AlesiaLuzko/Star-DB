4d2b14ad73513f33f571fd3c6cd7203888f18de5
Alesia Luzko <lesya.grishko@gmail.com>
2024-01-16 15:24:33 +0100

    withData: replace the class component to functional component

--- src/components/hoc-helpers/with-data.js
+++ src/components/hoc-helpers/with-data.js
@@ -1,48 +1,37 @@
-import React, {Component} from "react";
+import React, { useState, useEffect } from "react";
 import Spinner from "../spinner";
 import ErrorIndicator from "../error-indicator";
 
 const withData = (View) => {
-  return class extends Component {
-    state = {
-      data: null,
-      loading: true,
-      error: false
-    };
 
-    componentDidMount() {
-      this.setState( {
-        loading: true,
-        error: false
+  return ({getData, ...props}) => {
+
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+      setLoading(true);
+      setError(false);
+      getData().then((data) => {
+        setData(data);
+        setLoading(false);
       })
-      this.props.getData()
-        .then((data) => {
-          this.setState({
-            data,
-            loading: false
-          });
-        })
         .catch(() => {
-          this.setState({
-            error: true,
-            loading: false
-          });
+          setError(true);
+          setLoading(false);
         });
-    };
+    }, []);
 
-    render() {
-      const { data, loading, error } = this.state;
-
-      if(loading) {
-        return <Spinner/>
-      }
-
-      if(error) {
-        return <ErrorIndicator />
-      }
-
-      return <View {...this.props} data={data} />;
+    if(loading) {
+      return <Spinner/>
     }
+
+    if(error) {
+      return <ErrorIndicator />
+    }
+
+    return <View {...props} data={data} />;
   };
 };
 
